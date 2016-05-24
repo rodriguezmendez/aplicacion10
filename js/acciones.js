138b2d20feb6c769888ef4d641fd71c5e4ac5714
rodriguezmendez <misael3f.dsaupoo.cbtis@gmail.com>
2016-05-24 18:42:13 -0500

    2

--- js/acciones.js
+++ js/acciones.js
@@ -1,23 +1,25 @@
 // JavaScript Document
+
 var audio;
 $(document).ready(function(e) {
-    document.addEventListener("deviceready", function() {
+	
+    document.addEventListener("deviceready",function(){
 		
-		audio= window.plugins.LowLatencyAudio;
+		audio = window.plugins.LowLatencyAudio;
 		
-		audio.preloadFX('do', 'audio/C.mp3', function(msg){}, function(msg){alert('Error: ' + msg); });
-		audio.preloadFX('re', 'audio/D.mp3', function(msg){}, function(msg){alert('Error: ' + msg); });
-		audio.preloadFX('mi', 'audio/E.mp3', function(msg){}, function(msg){alert('Error: ' + msg); });
-		audio.preloadFX('fa', 'audio/F.mp3', function(msg){}, function(msg){alert('Error: ' + msg); });
-		audio.preloadFX('sol', 'audio/G.mp3', function(msg){}, function(msg){alert('Error: ' + msg); });
-		audio.preloadFX('la', 'audio/A.mp3', function(msg){}, function(msg){alert('Error: ' + msg); });
-		audio.preloadFX('si', 'audio/B.mp3', function(msg){}, function(msg){alert('Error: ' + msg); });
-		$('.nota').bind('touchstart', function(){
+		audio.preloadFX('do', 'audio/C.mp3',function(msg){},function(msg){alert('Error: ' + msg ); }); 
+		audio.preloadFX('re', 'audio/D.mp3',function(msg){},function(msg){alert('Error: ' + msg ); });
+		audio.preloadFX('mi', 'audio/E.mp3',function(msg){},function(msg){alert('Error: ' + msg ); });
+		audio.preloadFX('fa', 'audio/F.mp3',function(msg){},function(msg){alert('Error: ' + msg ); });
+		audio.preloadFX('sol', 'audio/G.mp3',function(msg){},function(msg){alert('Error: ' + msg ); });
+		audio.preloadFX('la', 'audio/A.mp3',function(msg){},function(msg){alert('Error: ' + msg ); });
+		audio.preloadFX('si', 'audio/B.mp3',function(msg){},function(msg){alert('Error: ' + msg ); });
+		
+		$('.nota').bind('touchstart' ,function(){
 			$(this).addClass('tocada');
 			audio.play($(this).attr('id'));
-		}).bind('touchend', function() {
+		}).bind('touchend', function(){
 			$(this).removeClass('tocada');
 		});
-	});
+		});
 });
-	
