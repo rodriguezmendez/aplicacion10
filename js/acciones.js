bfae6f4d0ca5d895e24c236456b19b5a481b6732
rodriguezmendez <misael3f.dsaupoo.cbtis@gmail.com>
2016-05-24 14:57:17 -0500

    1subida

---
+++ js/acciones.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+var audio;
+$(document).ready(function(e) {
+    document.addEventListener("deviceready", function() {
+		
+		audio= window.plugins.LowLatencyAudio;
+		
+		audio.preloadFX('do', 'audio/C.mp3', function(msg){}, function(msg){alert('Error: ' + msg); });
+		audio.preloadFX('re', 'audio/D.mp3', function(msg){}, function(msg){alert('Error: ' + msg); });
+		audio.preloadFX('mi', 'audio/E.mp3', function(msg){}, function(msg){alert('Error: ' + msg); });
+		audio.preloadFX('fa', 'audio/F.mp3', function(msg){}, function(msg){alert('Error: ' + msg); });
+		audio.preloadFX('sol', 'audio/G.mp3', function(msg){}, function(msg){alert('Error: ' + msg); });
+		audio.preloadFX('la', 'audio/A.mp3', function(msg){}, function(msg){alert('Error: ' + msg); });
+		audio.preloadFX('si', 'audio/B.mp3', function(msg){}, function(msg){alert('Error: ' + msg); });
+		$('.nota').bind('touchstart', function(){
+			$(this).addClass('tocada');
+			audio.play($(this).attr('id'));
+		}).bind('touchend', function() {
+			$(this).removeClass('tocada');
+		});
+	});
+});
+	
