f615d6fc18b63ddd94bf678a723adfe8aab3f184
rodriguezmendez <misael3f.dsaupoo.cbtis@gmail.com>
2016-05-26 13:50:11 -0500

    7

--- js/acciones.js
+++ js/acciones.js
@@ -14,6 +14,18 @@ $(document).ready(function(e) {
 		audio.preloadFX('sol', 'audio/SOL.mp3',function(msg){},function(msg){alert('Error: ' + msg ); });
 		audio.preloadFX('la', 'audio/LA.mp3',function(msg){},function(msg){alert('Error: ' + msg ); });
 		audio.preloadFX('si', 'audio/SI.mp3',function(msg){},function(msg){alert('Error: ' + msg ); });
+		audio.preloadFX('do1', 'audio/DO1.mp3',function(msg){},function(msg){alert('Error: ' + msg ); });
+		audio.preloadFX('do2', 'audio/DO1.mp3',function(msg){},function(msg){alert('Error: ' + msg ); });
+		audio.preloadFX('re1', 'audio/RE1.mp3',function(msg){},function(msg){alert('Error: ' + msg ); });
+		audio.preloadFX('re2', 'audio/RE1.mp3',function(msg){},function(msg){alert('Error: ' + msg ); });
+		audio.preloadFX('fa1', 'audio/FA1.mp3',function(msg){},function(msg){alert('Error: ' + msg ); });
+		audio.preloadFX('fa2', 'audio/FA1.mp3',function(msg){},function(msg){alert('Error: ' + msg ); });
+		audio.preloadFX('sol1', 'audio/SOL1.mp3',function(msg){},function(msg){alert('Error: ' + msg ); });
+		audio.preloadFX('sol2', 'audio/SOL1.mp3',function(msg){},function(msg){alert('Error: ' + msg ); });
+		audio.preloadFX('la1', 'audio/LA1.mp3',function(msg){},function(msg){alert('Error: ' + msg ); });
+		audio.preloadFX('la2', 'audio/LA1.mp3',function(msg){},function(msg){alert('Error: ' + msg ); });
+		
+		
 		
 		$('.nota').bind('touchstart' ,function(){
 			$(this).addClass('tocada');
@@ -21,5 +33,13 @@ $(document).ready(function(e) {
 		}).bind('touchend', function(){
 			$(this).removeClass('tocada');
 		});
+		
+		$('.nota1').bind('touchstart' ,function(){
+			$(this).addClass('tocada1');
+			audio.play($(this).attr('id'));
+		}).bind('touchend', function(){
+			$(this).removeClass('tocada1');
+		});
+		
 		});
 });
